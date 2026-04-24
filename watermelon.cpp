7d4a7a33b9ad0a85eee629d2889d5ae1891ef87e
Mohan Singh <7355184966mohan@gmail.com>
2026-04-24 13:26:17 +0000

    commit

---
+++ watermelon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    // Check if w is even AND greater than 2
+    if (w % 2 == 0 && w > 2) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
